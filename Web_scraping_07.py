e63de56c81251ecbc0164e5f165175895227a796
Jean <Jeankx@live.com>
2020-04-09 21:40:24 -0300

    09/04 as 21:40

---
+++ Web_scraping_07.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+import re
+import datetime
+import random
+
+
+pages = set()
+random.seed(datetime.datetime.now())
+
+
+def getInternalLinks(bs, includeUrl):
+    includeUrl = "{}://{}".format(urlparse(includeUrl).scheme, urlparse(includeUrl).netloc)
+    internalLinks = []
+
+    for link in bs.find_all("a", href=re.compile("^(/|.*"+includeUrl+")")):
+        if link.attrs["href"] is not None:
+            if link.attrs["href"] not in internalLinks:
+                if link.attrs["href"].startswitch("/"):
+                    internalLinks.append(includeUrl+link.attrs["href"])
+                else:
+                    internalLinks.append(link.attrs["href"])
+    return internalLinks
+
+def getExternalLinks(bs, excludeUrl):
+    externalLinks = []
+    for link in bs.find_all("a", href=re.compile("^(http|www)((?!"+excludeUrl+").)*$")):
+        if link.attrs["href"] is not None:
+
